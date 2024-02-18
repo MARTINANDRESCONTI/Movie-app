b5305920cf4a0a66c69452af5b77ed26cdc6677c
Martin Conti <matin.a.conti2014@gmail.com>
2024-02-17 22:29:25 -0300

    first commit

---
+++ src/app/components/header.jsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import martinIcon from '../../../public/martinIcon.svg';
+import style from './header.module.css'
+import Link from 'next/link'
+
+export default function Header() {  
+  return ( 
+      <header className={style.header}> 
+        <div className={style.title}>
+          <a className={style.icon} href='https://martin-conti-portfolio.vercel.app/' target='_blank'>
+            < Image
+              src={martinIcon}
+              className={style.logo}
+              alt="logo"
+            /> 
+          </a>
+          <h1>Movie App</h1>      
+        </div> 
+        <nav>
+          <div className={style.options}>
+            <Link className={style.option} href='/'><div>Home</div></Link>
+            <Link  className={style.option} href='/searchmovie'><div>Search</div></Link>  
+            <Link className={style.option} href='/favorites'><div >Favorites</div> </Link>             
+          </div>   
+        </nav>      
+      </header>
+  )
+}
+
